1d92ed024ce4e2977843b2626769db46a5c1218e
Cory Rodriguez <coreyreylp@gmail.com>
2014-11-26 11:25:47 -0600

    cleaned up old code

--- highlow.php
+++ highlow.php
@@ -6,34 +6,33 @@
 // correct guess leads to game over
 
 if ($argc == 3) {
-	$low = $argv[1];
-	$high = $argv[2];
-	$answer = mt_rand($low, $high);
+    $low = $argv[1];
+    $high = $argv[2];
+    $answer = mt_rand($low, $high);
 } else {
-	$answer = mt_rand(1, 100);
+    $answer = mt_rand(1, 100);
 }
 
 $guesses = 1;
 
 fwrite(STDOUT, "Let's play a game. Guess what number I am thinking of.\n");
 
-$user_guess = fgets(STDIN);
+$user_guess = trim(fgets(STDIN));
 
 while ($user_guess != $answer) {
-	// prompt user for another answer
-	// capture stdin
-	$guesses++; //tracks guesses for use later
+    // prompt user for another answer
+    // capture stdin
+    $guesses++; //tracks guesses for use later
 
-	if ($user_guess > $answer) {
-		echo "Lower.";
-	}
-	
-	elseif ($user_guess < $answer) {
-		echo "Higher.";
-	}
-	
-	fwrite(STDOUT, " Guess again.\n");
-	$user_guess = fgets(STDIN);
+    if ($user_guess > $answer) {
+        echo "Lower.";
+    } elseif ($user_guess < $answer) {
+        echo "Higher.";
+    }
+
+    $user_guess = trim(fgets(STDIN));
+
+    // fwrite(STDOUT, "Guess again.\n");
 
 } // end while statement
 
